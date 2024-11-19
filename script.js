caf32829382aabb699d45043738af9c0024a944b
nyoung90 <musicislovellc@gmail.com>
2024-11-19 04:50:56 +0000

    fixed errors in HTML and JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-getElementById('funfactButton').addEventListener('click', function() {
-  const funFact = document.getElementById('funfact');
+document.getElementById('funFactButton')
+.addEventListener('click', function() {
+  const funFact = document.getElementById('funFact');
   if (funFact.style.display === 'none') {
       funFact.style.display = 'block';
   } else {
